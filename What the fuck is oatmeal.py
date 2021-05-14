178a9711d7d59e1fe175645cc0c4f39c4445021b
YourPapaWhiskey <83650360+YourPapaWhiskey@users.noreply.github.com>
2021-05-14 13:59:56 -0400

    Create What the fuck is oatmeal.py

---
+++ What the fuck is oatmeal.py
@@ -0,0 +1,13 @@
+#What the fuck is oatmeal?
+import time
+print('What the fuck is oatmeal?')
+sleep.time(3)
+print('May nobody know the answer?')
+sleep.time(5)
+print('I may never know...')
+sleep.time(2)
+print('What the fuck is oatmeal.')
+sleep.time(1.5)
+userInput = input('Will you please tell me? \n')
+if userInput == 'Oats':
+    print('Thank you. I may finally rest easy.')
